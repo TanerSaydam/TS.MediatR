b797f130d58cbc66bfd1745348f27a564d0a7682
Taner Saydam <taner_and_you@hotmail.com>
2025-06-02 10:33:58 +0300

    Add project files.

---
+++ MediatROptions.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace TS.MediatR;
+public sealed class MediatROptions
+{
+    internal List<Assembly> Assemblies { get; set; } = new();
+    internal List<Type> PipelineBehaviors { get; set; } = new();
+
+    public void AddRegisterAssemblies(params Assembly[] assemblies)
+    {
+        Assemblies.AddRange(assemblies);
+    }
+
+    public void AddOpenBehavior(Type behaviorType)
+    {
+        PipelineBehaviors.Add(behaviorType);
+    }
+}
